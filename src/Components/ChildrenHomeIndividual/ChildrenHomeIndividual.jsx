68bbdb843da92f6fbc40b23d925aa727e9bd5c17
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-08-15 02:31:34 +0300

    initial commit

---
+++ src/Components/ChildrenHomeIndividual/ChildrenHomeIndividual.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChildrenHomeIndividual = () => {
+  return (
+    <div>ChildrenHomeIndividual</div>
+  )
+}
+
+export default ChildrenHomeIndividual
---
+++ src/Components/DonationVolunteeringFundraising/DonationVolunteeringFundraising.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DonationVolunteeringFundraising = () => {
+  return (
+    <div>DonationVolunteeringFundraising</div>
+  )
+}
+
+export default DonationVolunteeringFundraising
