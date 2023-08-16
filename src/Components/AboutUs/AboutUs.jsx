1193d0ea51f7f3ef98d2323128aa62180e3549d5
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-08-16 19:45:03 +0300

    Added the Navbar

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Navbar from "../Navbar/Navbar"
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="logo">
+        <h1>
+          Chang<span className="ia">ia</span>
+        </h1>
+      </div>
 
-export default Navbar
+      <div className="options">
+        <ul className="menu-items">
+          <li>
+            <Link className="nav-links" to="/AboutUs">
+              About us
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-links" to="/DonationVolunteeringFundraising">
+              Discover
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-links" to="/SearchResults">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-links" to="/Blog">
+              Blog
+            </Link>
+          </li>
+          <li className="sign-up button">
+            <Link className="nav-links" to="/Register">
+              Sign Up
+            </Link>
+          </li>
+          <li className="sign-in button">
+            <Link className="nav-links" to="/LogIn">
+              Sign in
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
