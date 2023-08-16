2c92567e7fdeb47683e4d804984e5271661deba1
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-08-16 20:49:15 +0300

    added content component

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,11 +1,14 @@
-import Navbar from "../Navbar/Navbar"
+import Navbar from "../Navbar/Navbar";
+import "./aboutUs.css";
 
 const AboutUs = () => {
   return (
     <div>
       <Navbar />
+      <div className="banner"></div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
