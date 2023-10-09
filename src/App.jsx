407c6b6b6a8d89720d0fe8e3e0fc31a2557c3939
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-10-09 13:39:16 +0300

    chnaged the router to a data API router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 // Page imports
-import LogIn from "./Components/LogIn/LogIn"
-import Register from "./Components/Register/Register"
-import Home from "./Components/Home/Home"
-import AboutUs from "./Components/AboutUs/AboutUs"
-import Article from "./Components/Article/Article"
+import LogIn from "./Components/LogIn/LogIn";
+import Register from "./Components/Register/Register";
+import Home from "./Components/Home/Home";
+import AboutUs from "./Components/AboutUs/AboutUs";
+import Article from "./Components/Article/Article";
 import Blog from "./Components/Blog/Blog";
 import ChildrenHomeIndividual from "./Components/ChildrenHomeIndividual/ChildrenHomeIndividual";
 import DonationVolunteeringFundraising from "./Components/DonationVolunteeringFundraising/DonationVolunteeringFundraising";
 import SearchResults from "./Components/SearchResults/SearchResults";
 
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  const location = useLocation();
-
-  return (
-    <div className="app">
-      <Routes key={location.pathname} location={location}>
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route key={location.pathname} location={location}>
         <Route path="/" element={<Home />} />
         <Route path="/LogIn" element={<LogIn />} />
         <Route path="/Register" element={<Register />} />
@@ -35,9 +37,11 @@ function App() {
           element={<DonationVolunteeringFundraising />}
         />
         <Route path="/SearchResults" element={<SearchResults />} />
-      </Routes>
-    </div>
+      </Route>
+    )
   );
+
+  return <RouterProvider router={routes} />;
 }
 
-export default App
+export default App;
