0f18e4ca7390af6929439da458e34338afe5893f
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-10-09 14:47:59 +0300

    fixes to the boiler plate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+} from "react-router-dom";
 
 // Page imports
 import LogIn from "./Components/LogIn/LogIn";
@@ -12,9 +16,7 @@ import Blog from "./Components/Blog/Blog";
 import ChildrenHomeIndividual from "./Components/ChildrenHomeIndividual/ChildrenHomeIndividual";
 import SearchResults from "./Components/SearchResults/SearchResults";
 
-
-
-import './App.css'
+import "./App.css";
 import Donation from "./Components/Impact/Donate/Donation";
 import Fundraising from "./Components/Impact/Fundraise/Fundraising";
 import Volunteering from "./Components/Impact/Volunteer/Volunteering";
@@ -22,37 +24,28 @@ import ImpactLayout from "./Components/Impact/ImpactLayout";
 import AppLayout from "./AppLayout";
 
 function App() {
-  const location = useLocation();
-
-  return (
-    <div className="app">
-      <Routes key={location.pathname} location={location}>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/LogIn" element={<LogIn />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/Article" element={<Article />} />
-          <Route
-            path="/individualChildrenHome"
-            element={<ChildrenHomeIndividual />}
-          />
-          <Route path="/Blog" element={<Blog />} />
-          {/* <Route
-            path="/DonationVolunteeringFundraising"
-            element={<DonationVolunteeringFundraising />}
-          /> */}
-          <Route path="/SearchResults" element={<SearchResults />} />
-          <Route path="/Impact" element={<ImpactLayout />}>
-            <Route index element={<Navigate replace to="donate" />} />
-            <Route path="donate" element={<Donation />} />
-            <Route path="fundraise" element={<Fundraising />} />
-            <Route path="volunteer" element={<Volunteering />} />
-          </Route>
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Article" element={<Article />} />
+        <Route
+          path="/individualChildrenHome"
+          element={<ChildrenHomeIndividual />}
+        />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/SearchResults" element={<SearchResults />} />
+        <Route path="/Impact" element={<ImpactLayout />}>
+          <Route index element={<Navigate replace to="donate" />} />
+          <Route path="donate" element={<Donation />} />
+          <Route path="fundraise" element={<Fundraising />} />
+          <Route path="volunteer" element={<Volunteering />} />
         </Route>
-      </Routes>
-    </div>
-
+      </Route>
+    )
   );
 
   return <RouterProvider router={routes} />;
