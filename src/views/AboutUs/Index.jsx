39d14326bb3f8559f1cc38134f324cd3cc171460
Richard Githuba <githuba9520@gmail.com>
2024-07-03 22:28:56 +0300

    done with the about us page

--- src/views/AboutUs/Index.jsx
+++ src/views/AboutUs/Index.jsx
@@ -1,10 +1,12 @@
 import { Flex } from 'antd';
 import AboutContent from './Components/AboutContent';
+import AboutForm from './Components/AboutForm';
 
 const AboutUs = () => {
   return (
     <Flex vertical className='about-container'>
       <AboutContent />
+      <AboutForm />
     </Flex>
   );
 };
