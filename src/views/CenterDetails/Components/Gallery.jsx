85001297373b50d6b097fac0bb3dc21a04dd683c
Richard Githuba <githuba9520@gmail.com>
2024-07-10 21:07:07 +0300

    added shadcdn

--- src/views/CenterDetails/Components/Gallery.jsx
+++ src/views/CenterDetails/Components/Gallery.jsx
@@ -1,16 +1,32 @@
-import { Flex, Typography } from "antd"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Card, Flex, Typography } from "antd"
 
 const {Title} = Typography
 const Gallery = () => {
   return (
-    <Flex vertical className="gallery-container">
-        <Title >
-            <span>
-                Gallery
-            </span>
-        </Title>
+    <Flex vertical className='gallery-container'>
+      <Title>
+        <span>Gallery</span>
+      </Title>
+      <Carousel className='w-full max-w-xs'>
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className='p-1'>
+                <Card>
+                  <div className='flex aspect-square items-center justify-center p-6'>
+                    <span className='text-4xl font-semibold'>{index + 1}</span>
+                  </div>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </Flex>
-  )
+  );
 }
 
 export default Gallery
