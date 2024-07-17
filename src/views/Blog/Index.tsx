3ab796a5da7449db7aad3cc97d5dbbb364a0e25a
githubarj <githubarj@gmail.com>
2024-07-17 18:19:13 +0300

    added the blog page route

---
+++ src/views/Blog/Index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+	import {Flex} from 'antd'
+
+const Bio : React.FC = () => {
+	return (
+
+		<Flex vertical className= 'blog'>
+		
+		</Flex>
+
+	)
+}
+
+
+export default Bio;
