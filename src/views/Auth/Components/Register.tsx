5a3d90d5dad441a30b8645e8e9e629c6c3f832f3
githubarj <githubarj@gmail.com>
2024-07-17 14:47:56 +0300

    added the registration page

--- src/views/Auth/Components/Register.tsx
+++ src/views/Auth/Components/Register.tsx
@@ -2,38 +2,38 @@ import {Col , Row, Typography, Flex} from 'antd'
 import { primaryBrown } from "@/helpers/colors";
 import { FaFacebook } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
+import registerPic from '../assets/register.jpeg'
+import RegisterForm from './RegisterForm'
 
 
 const {Title ,Text , Link } = Typography
 const Register = () => {
-
-
     return ( 
     <Row className="sign-in" >
       <Col span={12} className="sign-in-pic">
-        {/*<img src={signUp} alt="" />*/}
+        <img src={registerPic} alt="" />
       </Col>
       <Col md={12} sm={24} lg={12} xl={12} className="form-side">
         <Title>
           Welcome to <span>Chang</span>ia
         </Title>
-        
+        <RegisterForm />
         
         <Flex justify={"center"} vertical align="center" gap={"middle"}>
-          <Text>
+         {/* <Text>
             <span style={{ color: primaryBrown }}>OR</span> Login with
           </Text>
           <Flex gap={"large"}>
             <FaFacebook size={24} color="blue" />
             <FcGoogle size={26} />
-          </Flex>
+          </Flex>*/}
           <Text>
-            Don't have an account ?{" "}
+            Already have an account ?{" "}
             <Link
               style={{ color: primaryBrown }}
-              onClick={() => navigateTo("/auth/sign-up")}
+              onClick={() => navigateTo("/auth/sign-in")}
             >
-              Sign Up
+              Sign In
             </Link>
           </Text>
         </Flex>
